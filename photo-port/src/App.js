71e4bf81786561f76bf5b010a3cca5fc4bb26bc8
bokigolic <bokigolic32@gmail.com>
2022-09-22 13:39:46 -0400

    add component About

--- photo-port/src/App.js
+++ photo-port/src/App.js
@@ -1,11 +1,15 @@
+import React from 'react';
 
+import './App.css';
+import About from './components/about';
 
 function App() {
   return (
-    <div className="App">
-
+    <div>
+      <main>
+        <About/>
+      </main>
     </div>
   );
 }
-
 export default App;
